e5427f52fefdfb5ff99cd7167ff454327d217570
Extension Team <106770188+extensionteamhq@users.noreply.github.com>
2024-04-16 14:35:52 -0400

    modified styles to work in both modes

--- src/lib/components/sections/InfoBanner_01.svelte
+++ src/lib/components/sections/InfoBanner_01.svelte
@@ -8,7 +8,7 @@
     // node modules
 
     // local
-    import { layoutConfig } from '../../data/layout';
+    import { layoutConfig } from '$lib/data/layout';
     import { isScroll } from '$lib/store';
     // Access functions from the context
 
@@ -27,7 +27,7 @@
         setTimeout(() => {
             showBanner = true;
             isFirstRender = false;
-        }, 5000);
+        }, 2500);
     });
 </script>
 
@@ -51,18 +51,23 @@
                         </path>
                     </svg>
                     <p class="text-sm md:text-base leading-6 p-0 m-0 text-center"
-                        ><span class="font-bold">Going to SquadUp Summit?</span> Message me on
+                        >Going to
+                        <a
+                            href="https://www.squadupsummit.com/"
+                            target="_blank"
+                            class="font-bold underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">SquadUp Summit</a
+                        >? Message me on
                         <a
                             href="https://instagram.com/{layoutConfig.site.socialHandle_02}"
                             target="_blank"
-                            class="border p-0.5 mx-0.5 pr-1 whitespace-nowrap rounded text-neutral-900 bg-neutral-300 hover:text-neutral-100 hover:bg-neutral-900 dark:hover:text-neutral-900 dark:hover:bg-neutral-100"
-                            ><span class=" [&>svg]:h-5 [&>svg]:w-5">
+                            class="px-0.5 pr-1 py-1 mx-0.5 whitespace-nowrap rounded-md border border-neutral-100 dark:border-neutral-900 text-neutral-900 bg-neutral-100 hover:text-neutral-100 hover:bg-neutral-900 dark:text-neutral-100 dark:bg-neutral-900 dark:hover:text-neutral-900 dark:hover:bg-neutral-100">
+                            <span class=" [&>svg]:h-5 [&>svg]:w-5">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="inline-block pb-0.5">
                                     <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com/icons/instagram?f=brands&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
                                     <path
                                         d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
                                 </svg>
-                            </span>Instagram</a> if you would like to connect.</p>
+                            </span> Instagram</a> if you would like to connect.</p>
                     <button on:click={onClose} type="button" class="items-center rounded-lg text-sm p-1.5 mx-2">
                         <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path

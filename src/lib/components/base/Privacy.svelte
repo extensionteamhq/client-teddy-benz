f102af73434106e2f5c3ab1e880376789dad7a45
Extension Team <106770188+extensionteamhq@users.noreply.github.com>
2024-04-18 10:28:05 -0400

    updated privacy policy content

--- src/lib/components/base/Privacy.svelte
+++ src/lib/components/base/Privacy.svelte
@@ -1,18 +1,86 @@
-<div class="grid min-h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
-  <div>
-    <p class="text-base font-semibold">
-      Privacy Policy
-    </p>
-    <h1 class="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-      Privacy Policy
-    </h1>
-    <p class="mt-6 text-base leading-7">
-      Privacy Policy
-    </p>
-    <div class="mt-10 flex items-center justify-center gap-x-6">
-      <a href="/" class="rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm text-white bg-gray-700 hover:bg-gray-400 focus-visible:outline-gray-400 focus-visible:outline">
-        Go To Our Home Page
-      </a>
+<script lang="ts">
+    // svelte core
+
+    // svelte plugins
+    import { getContext } from 'svelte';
+    // other plugins and modules
+
+    // local
+    import { layoutConfig } from '$lib/data/layout';
+</script>
+
+<div class="mx-auto pt-44 mb-10" id="privacy">
+    <div class="grid min-h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div>
+            <p class="text-base font-semibold"> Privacy Policy </p>
+            <h1 class="text-3xl font-bold my-6"> Privacy Policy </h1>
+            <p>Effective date: April 15th, 2024</p>
+
+            <p
+                >{layoutConfig.site.siteName}, it's representatives, and associates (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;) is committed to
+                protecting your privacy. This Privacy Policy explains how we collect, use, protect, and handle your personal information on {layoutConfig.site
+                    .siteURL}.</p>
+
+            <p class="font-bold">1. Information We Collect</p>
+
+            <p
+                >We collect information from you when you voluntarily provide us with such information, including but not limited to your name, phone number,
+                and email address. This occurs when you:</p>
+
+            <ul class="list-disc">
+                <ol>Sign up for our newsletter</ol>
+                <ol>Contact us for inquiries</ol>
+            </ul>
+
+            <p class="font-bold">2. Use of Information</p>
+
+            <p>The information we collect from you may be used in the following ways:</p>
+
+            <ul class="list-disc">
+                <ol>To personalize your experience and respond to your individual needs</ol>
+                <ol>To improve our website and services</ol>
+                <ol>To contact you with updates, promotional offers, and other information related to our services</ol>
+            </ul>
+
+            <p class="font-bold">3. Protection of Information</p>
+
+            <p
+                >We implement a variety of security measures to maintain the safety of your personal information when you enter, submit, or access your personal
+                information. These measures comply with best practices in the industry.</p>
+
+            <p class="font-bold">4. Disclosure of Information</p>
+
+            <p
+                >We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third
+                parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this
+                information confidential.</p>
+
+            <p class="font-bold">5. Opt-Out Rights</p>
+
+            <p
+                >If at any time you would like to unsubscribe from receiving future emails or phone communications, you can <a
+                    href="/#contact"
+                    class="font-bold underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">contact us</a> and we will promptly
+                remove you from all correspondence.</p>
+
+            <p class="font-bold">6. Contacting Us</p>
+
+            <p
+                >If there are any questions regarding this privacy policy, you may <a
+                    href="/#contact"
+                    class="font-bold underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">contact us</a
+                >.</p>
+
+            <p class="font-bold">Changes to our Privacy Policy</p>
+
+            <p>If we decide to change our privacy policy, we will post those changes on this page and/or update the Privacy Policy modification date above.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                    href="/"
+                    class="rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm text-white bg-gray-700 hover:bg-gray-400 focus-visible:outline-gray-400 focus-visible:outline">
+                    Go To Our Home Page
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -2,79 +2,104 @@
     // svelte core
 
     // svelte plugins
-
-    // flowbite plugins
-
-    // node modules
+    import { getContext } from 'svelte';
+    // other plugins and modules
 
     // local
+    import { layoutConfig } from '$lib/data/layout';
 </script>
 
 <svelte:head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>About</title>
-  <meta property="og:title" content="" />
-  <meta name="description" content="" />
-  <meta property="og:description" content="" />
-  <meta name="author" content="" />
-  <meta property="og:image" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:type" content="website" />
-  <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "Organization",
-      "name": "",
-      "address": {
-        "@type": "PostalAddress",
-        "addressCountry": "",
-        "postalCode": "",
-        "addressRegion": "",
-        "addressLocality": ""
-      },
-      "description": "",
-      "image": "",
-      "email": "",
-      "url": ""
-    }
-  </script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>About</title>
+    <meta property="og:title" content="" />
+    <meta name="description" content="" />
+    <meta property="og:description" content="" />
+    <meta name="author" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="website" />
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "",
+            "address": {
+                "@type": "PostalAddress",
+                "addressCountry": "",
+                "postalCode": "",
+                "addressRegion": "",
+                "addressLocality": ""
+            },
+            "description": "",
+            "image": "",
+            "email": "",
+            "url": ""
+        }
+    </script>
 </svelte:head>
 
 <!-- The `container` class sets the max-width of an element to match the min-width of the current breakpoint. This is useful if you’d prefer to design for a fixed set of screen sizes instead of trying to accommodate a fully fluid viewport. -->
-<main class="container mx-auto px-10 sm:px-0 mt-24 mb-10" id="home">
-    <h1 class="text-3xl font-bold my-6">
-        Privacy Policy
-    </h1>
-    <p>
-      Porta ornare nisi cum faucibus lacinia class blandit vehicula consectetur. Neque natoque purus tortor. Cubilia tempor vestibulum aenean diam. Porttitor fusce, nisi porta porta venenatis magna. Litora vestibulum auctor platea morbi sem, urna facilisi dis? Aptent facilisi convallis ullamcorper dictumst blandit platea porta ornare posuere imperdiet. Placerat urna mollis velit. Risus at libero sem leo pretium sociosqu at eget erat blandit mi commodo. Sem aenean nascetur himenaeos. Mollis blandit imperdiet ut elit consequat ante posuere natoque ornare blandit venenatis. Feugiat urna magna pharetra himenaeos ad. Habitant auctor risus nam ut quam dictum ligula et auctor nascetur dis interdum. Feugiat nullam torquent feugiat cras. Condimentum rhoncus odio condimentum.
-    </p>
-    <p>
-      Elementum amet convallis neque ligula facilisis eros condimentum dictumst risus sapien. Quam habitant sociosqu dignissim, torquent cras augue ornare volutpat mollis nisi et. Mattis quisque egestas faucibus phasellus urna eu venenatis aliquam tellus dis ultricies egestas. Suspendisse montes purus accumsan quis cursus torquent montes mauris? Ante hendrerit sem vivamus mollis venenatis pulvinar vitae vehicula.
-    </p>
-    <p>
-      Fringilla vitae velit ultrices hac quam. Ullamcorper posuere, dolor metus class eget tortor senectus vitae parturient feugiat varius netus. Ante justo scelerisque augue proin, consectetur orci torquent! Morbi cras porttitor purus parturient vehicula inceptos augue id vehicula. Tortor leo morbi faucibus lacinia fames turpis penatibus leo. Sociis rhoncus dui orci, dui est. Et, penatibus scelerisque cubilia cras magnis molestie praesent dis himenaeos molestie pharetra. Malesuada et turpis senectus torquent himenaeos habitasse faucibus! Lacus amet purus nullam. Nibh venenatis senectus cras orci luctus sagittis. Primis posuere aliquam gravida suspendisse, mi venenatis dictum fusce praesent imperdiet parturient mi. Potenti class pharetra curae; aliquet quis ac placerat amet conubia lorem. Porttitor ultricies nunc cras praesent lectus tortor condimentum a mi senectus ut gravida? Suscipit arcu penatibus ligula, natoque dui. Congue tincidunt euismod molestie convallis lobortis lacus sed leo vitae ornare cum. Porttitor dapibus aptent proin. Habitant vivamus tortor penatibus.
-    </p>
-    <p>
-      Vel, metus augue elementum mauris quis suscipit vehicula imperdiet felis. Suscipit taciti aliquam leo bibendum? At purus sem augue suspendisse. Sit odio dictumst, ac potenti integer. Cubilia pulvinar rhoncus mus integer purus dictumst ligula rutrum sed phasellus. Taciti laoreet per vivamus lacus consectetur! Cubilia quis turpis nascetur lectus. Ridiculus libero aliquam proin tellus. Lorem mi est sociis risus porta. Fames, nibh luctus tristique. A id dictum risus ante nisi luctus? Tempus tincidunt tincidunt, mattis porttitor etiam metus. Orci facilisi sodales facilisis montes tincidunt vestibulum fames felis hendrerit vehicula. Aenean tristique pretium condimentum nam. Id fringilla platea facilisi nisi est nascetur ad cubilia sociosqu. Aliquet aptent rhoncus natoque hac dapibus. Integer dictum augue ullamcorper erat. Primis ultrices vel felis.
-    </p>
-    <p>
-      Tortor lobortis, velit lorem suspendisse mauris nisi iaculis diam tortor? Tortor quam accumsan risus magnis. Nisl hac eu senectus. Elit praesent sodales montes tempor mi himenaeos, primis per purus. Inceptos condimentum eget ligula. Auctor per dui netus elit suscipit. Nibh mus convallis parturient arcu per! Morbi phasellus felis ultricies duis ut. Torquent per habitasse taciti purus lectus tristique mi sollicitudin venenatis consectetur? Magna consectetur at scelerisque quam montes quam aptent fusce dui. Eget, inceptos vel nascetur morbi! Convallis in ultricies bibendum vivamus vestibulum arcu metus duis! Bibendum penatibus at nunc sociis tristique ipsum mauris, rhoncus tellus. Mollis lorem id?
-    </p>
-    <p>
-      Commodo dictum sit pellentesque malesuada molestie himenaeos. Imperdiet vel cursus aliquam! Gravida, sagittis metus in accumsan. Sapien pulvinar nec, molestie himenaeos ipsum aptent? Ultricies facilisis at est. Diam posuere nullam sed dapibus eros urna! Sociosqu iaculis tempus dui rhoncus habitasse vivamus tempor dui nostra quisque cras? Eu faucibus vel, suspendisse facilisis gravida neque eros. Dignissim sociis urna sit dis. Iaculis volutpat pharetra, tristique in adipiscing dignissim platea lobortis venenatis risus taciti. Ligula pulvinar nullam inceptos. Magnis nascetur nisi turpis justo vulputate. Diam accumsan lacus elementum magna rhoncus. Integer venenatis mauris non vivamus. Ipsum rhoncus nisi odio mattis. Elit habitant, magna netus ad. Mauris libero cum venenatis turpis at odio, dictum dolor vehicula sodales sed. Rutrum sociosqu luctus quisque.
-    </p>
-    <p>
-      Sollicitudin senectus, suspendisse viverra. Ipsum torquent imperdiet in curabitur habitant molestie. Nullam potenti ultricies aliquam mauris nostra ridiculus facilisis litora tellus consectetur nibh. Inceptos in platea ultricies magnis augue orci orci class? Amet venenatis cursus eget inceptos vivamus. Lectus ipsum ornare dis litora duis! Nisi pretium, placerat duis nisl aliquam. Commodo conubia nisl at in sit commodo nostra eu consectetur suspendisse ligula? Mollis semper consequat, taciti amet? Diam elementum laoreet bibendum? Erat netus, blandit suscipit primis congue porta. Tempus arcu nulla massa aliquam orci turpis ornare habitant lorem dui. Cras platea iaculis urna dis phasellus leo.
-    </p>
-    <p>
-      Egestas parturient varius dolor faucibus elit orci volutpat congue commodo porta. Porttitor aliquam volutpat, lorem fusce. Proin egestas, inceptos auctor leo congue in quam habitant! Pretium consectetur curabitur varius vel orci iaculis consectetur tempus? Libero leo non euismod, primis nulla dapibus. Nascetur vehicula nullam turpis taciti nascetur viverra dolor placerat! Himenaeos morbi imperdiet arcu pulvinar potenti. Luctus cursus imperdiet tempor porta suscipit mauris hac vivamus turpis sem erat leo. Hendrerit potenti lorem condimentum congue, sit faucibus fermentum lacus. Mauris dis velit aenean. Accumsan cubilia tempus tortor varius praesent ornare molestie. Congue blandit nisl ornare hac. Sed magnis vulputate fusce libero. Ante tempus aenean ipsum? Dolor tristique augue felis congue integer risus dictumst mus. Venenatis turpis duis metus per primis neque. Penatibus consequat sodales posuere? Felis luctus feugiat class vestibulum in per. Sollicitudin mauris massa, consectetur dictumst elementum. Tortor per nunc aliquet facilisis elit elit.
-    </p>
-    <p>
-      Senectus hac semper pretium. Bibendum elit primis nisl hendrerit. Facilisi elit urna aliquet ac eros taciti lobortis nunc fames lorem sociosqu! Dis ante et cursus bibendum nibh elit sociis, vel netus. Montes proin sed vehicula mauris, arcu egestas. Fringilla auctor orci nisl inceptos ornare nisi vivamus dignissim rhoncus netus. Phasellus auctor luctus vitae ullamcorper? Lacinia sapien diam nisi. Pretium aliquet habitant mollis magnis.
-    </p>
-    <p>
-      Vitae proin inceptos mus cubilia fermentum? Nisi mollis curae; quam quis sed luctus consectetur rhoncus quisque leo. Phasellus, aliquam interdum class? Vel dignissim pulvinar himenaeos dignissim aliquet eget senectus. Fames ac netus fermentum accumsan taciti porta. Commodo tristique, gravida nostra mollis et ultrices ante. Porttitor facilisis magna ornare lobortis eros vivamus. Felis tristique diam sociis volutpat posuere rhoncus ut. Lectus turpis montes imperdiet, viverra dignissim quam conubia leo? Sit sed condimentum augue. Enim potenti gravida facilisi luctus condimentum fusce tellus senectus suscipit magna fusce. Rhoncus cursus eros, scelerisque euismod. Natoque duis, quam consequat. Malesuada nisl hendrerit viverra cursus cursus tellus class. Dignissim mollis ornare parturient, adipiscing leo laoreet condimentum sodales? Fringilla sed ipsum.
-    </p>
-</main>
+<div class="container mx-auto pt-44 mb-10" id="privacy">
+    <h1 class="text-3xl font-bold my-6"> Privacy Policy </h1>
+    <p>Effective date: April 15th, 2024</p>
+
+    <p
+        >{layoutConfig.site.siteName}, it's representatives, and associates (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;) is committed to protecting your
+        privacy. This Privacy Policy explains how we collect, use, protect, and handle your personal information on {layoutConfig.site.siteURL}.</p>
+
+    <p class="font-bold">1. Information We Collect</p>
+
+    <p
+        >We collect information from you when you voluntarily provide us with such information, including but not limited to your name, phone number, and email
+        address. This occurs when you:</p>
+
+    <ul class="list-disc">
+        <ol>Sign up for our newsletter</ol>
+        <ol>Contact us for inquiries</ol>
+    </ul>
+
+    <p class="font-bold">2. Use of Information</p>
+
+    <p>The information we collect from you may be used in the following ways:</p>
+
+    <ul class="list-disc">
+        <ol>To personalize your experience and respond to your individual needs</ol>
+        <ol>To improve our website and services</ol>
+        <ol>To contact you with updates, promotional offers, and other information related to our services</ol>
+    </ul>
+
+    <p class="font-bold">3. Protection of Information</p>
+
+    <p
+        >We implement a variety of security measures to maintain the safety of your personal information when you enter, submit, or access your personal
+        information. These measures comply with best practices in the industry.</p>
+
+    <p class="font-bold">4. Disclosure of Information</p>
+
+    <p
+        >We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties
+        who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential.</p>
+
+    <p class="font-bold">5. Opt-Out Rights</p>
+
+    <p
+        >If at any time you would like to unsubscribe from receiving future emails or phone communications, you can <a
+            href="/#contact"
+            class="font-bold underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">contact us</a> and we will promptly remove
+        you from all correspondence.</p>
+
+    <p class="font-bold">6. Contacting Us</p>
+
+    <p
+        >If there are any questions regarding this privacy policy, you may <a
+            href="/#contact"
+            class="font-bold underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">contact us</a
+        >.</p>
+
+    <p class="font-bold">Changes to our Privacy Policy</p>
+
+    <p>If we decide to change our privacy policy, we will post those changes on this page and/or update the Privacy Policy modification date above.</p>
+</div>

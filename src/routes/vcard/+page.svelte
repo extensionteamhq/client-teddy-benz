a08757e678c6c913906ee8c8e68a30d37e783b26
Extension Team <106770188+extensionteamhq@users.noreply.github.com>
2024-04-22 13:44:12 -0400

    updated vcard page

--- src/routes/vcard/+page.svelte
+++ src/routes/vcard/+page.svelte
@@ -11,31 +11,33 @@
     import { layoutConfig } from '$lib/data/layout';
 </script>
 
-<div class="px-6 py-32 lg:px-8">
+<div class="px-6 py-32 lg:px-8 mt-20">
     <div class="mx-auto max-w-3xl text-base leading-7">
         <p class="text-base font-semibold leading-7">My Digital Business Card</p>
         <!-- vcard header -->
         <div class="grid grid-cols-2 grid-rows-1 mb-8 gap-x-10">
             <!-- name, title, company -->
             <div>
-                <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    {#if layoutConfig.site.titleName != ''}
-                        {layoutConfig.site.titleName}
-                    {/if}
-                    {#if layoutConfig.site.firstName != ''}
-                        {layoutConfig.site.firstName}
-                    {/if}
-                    {#if layoutConfig.site.middleName != ''}
-                        {layoutConfig.site.middleName}
-                    {/if}
-                    {#if layoutConfig.site.lastName != ''}
-                        {layoutConfig.site.lastName}
-                    {/if}
-                    {#if layoutConfig.site.suffixName != ''}
-                        {layoutConfig.site.suffixName}
-                    {/if}
-                </h1>
-                {#if layoutConfig.site.nickName != '' || layoutConfig.site.orgTitle != '' || layoutConfig.site.orgName != ''}
+                {#if layoutConfig.site.titleName != '' || layoutConfig.site.firstName != '' || layoutConfig.site.middleName != '' || layoutConfig.site.lastName != '' || layoutConfig.site.suffixName != ''}
+                    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        {#if layoutConfig.site.titleName != ''}
+                            {layoutConfig.site.titleName}
+                        {/if}
+                        {#if layoutConfig.site.firstName != ''}
+                            {layoutConfig.site.firstName}
+                        {/if}
+                        {#if layoutConfig.site.middleName != ''}
+                            {layoutConfig.site.middleName}
+                        {/if}
+                        {#if layoutConfig.site.lastName != ''}
+                            {layoutConfig.site.lastName}
+                        {/if}
+                        {#if layoutConfig.site.suffixName != ''}
+                            {layoutConfig.site.suffixName}
+                        {/if}
+                    </h1>
+                {/if}
+                {#if layoutConfig.site.nickName != '' || layoutConfig.site.orgName != ''}
                     <p class="mt-1 mb-0">
                         {#if layoutConfig.site.nickName != ''}
                             <span class="font-bold italic">&quot;{layoutConfig.site.nickName}&quot;</span>
@@ -44,9 +46,6 @@
                         {#if layoutConfig.site.orgTitle != ''}
                             {layoutConfig.site.orgTitle}
                             <br />
-                        {/if}
-                        {#if layoutConfig.site.orgName != ''}
-                            {layoutConfig.site.orgName}
                         {/if}
                     </p>
                 {/if}
@@ -72,73 +71,99 @@
             </div>
         </div>
         <!--  business -->
-        <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
-            <div class="flex content-center items-center">
-                <span class="[&>svg]:h-10 [&>svg]:w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 384 512">
-                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/building?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                        <path
-                            d="M48 0C21.5 0 0 21.5 0 48V464c0 26.5 21.5 48 48 48h96V432c0-26.5 21.5-48 48-48s48 21.5 48 48v80h96c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H48zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm112-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM80 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V112zM272 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16z" />
-                    </svg>
-                </span>
+        {#if layoutConfig.site.orgName || layoutConfig.site.addStreet1_02 || layoutConfig.site.addStreet2_02 || layoutConfig.site.addCity_02 || layoutConfig.site.addRegion_02 || layoutConfig.site.addPostalCode_02 !== ''}
+            <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
+                <div class="flex content-center items-center">
+                    <span class="[&>svg]:h-10 [&>svg]:w-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 384 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/building?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M48 0C21.5 0 0 21.5 0 48V464c0 26.5 21.5 48 48 48h96V432c0-26.5 21.5-48 48-48s48 21.5 48 48v80h96c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H48zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm112-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM80 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V112zM272 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16z" />
+                        </svg>
+                    </span>
+                </div>
+                <div class="col-span-11">
+                    <p class="m-0">
+                        {#if layoutConfig.site.orgName !== ''}
+                            {layoutConfig.site.orgName}
+                            <br />
+                        {/if}
+                        {#if layoutConfig.site.addStreet1_02 !== ''}
+                            {layoutConfig.site.addStreet1_02}
+                        {/if}
+                        {#if layoutConfig.site.addStreet2_02 !== ''}
+                            {layoutConfig.site.addStreet2_02}
+                            <br />
+                        {/if}
+                        {#if layoutConfig.site.addCity_02 !== ''}
+                            {layoutConfig.site.addCity_02}
+                        {/if}
+                        {#if layoutConfig.site.addCity_02 && layoutConfig.site.addRegion_02 !== ''}
+                            , {layoutConfig.site.addRegion_02}
+                        {:else}
+                            {layoutConfig.site.addRegion_02}
+                        {/if}
+                        {#if layoutConfig.site.addPostalCode_02 !== ''}
+                            {layoutConfig.site.addPostalCode_02}
+                        {/if}
+                    </p>
+                </div>
             </div>
-            <div class="col-span-11">
-                <p class="m-0">
-                    {layoutConfig.site.addStreet1_02}, {layoutConfig.site.addStreet2_02}
-                    <br />
-                    {layoutConfig.site.addCity_02}, {layoutConfig.site.addRegion_02}
-                    {layoutConfig.site.addPostalCode_02}, {layoutConfig.site.addCountry_02}
-                    <br />
-                    {layoutConfig.site.urlAdd_02}
-                    <br />
-                    {layoutConfig.site.telNumber_02}
-                    <br />
-                    {layoutConfig.site.emailAdd_02}
-                </p>
-            </div>
-        </div>
+        {/if}
         <!-- home -->
-        <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
-            <div class="flex content-center items-center">
-                <span class="[&>svg]:h-10 [&>svg]:w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512">
-                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/house?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                        <path
-                            d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
-                    </svg>
-                </span>
+        {#if layoutConfig.site.addStreet1_01 || layoutConfig.site.addStreet2_01 || layoutConfig.site.addCity_01 || layoutConfig.site.addRegion_01 || layoutConfig.site.addPostalCode_01 !== ''}
+            <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
+                <div class="flex content-center items-center">
+                    <span class="[&>svg]:h-10 [&>svg]:w-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/house?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+                        </svg>
+                    </span>
+                </div>
+                <div class="col-span-11">
+                    <p class="m-0">
+                        {#if layoutConfig.site.addStreet1_01 !== ''}
+                            {layoutConfig.site.addStreet1_01}
+                        {/if}
+                        {#if layoutConfig.site.addStreet2_01 !== ''}
+                            {layoutConfig.site.addStreet2_01}
+                            <br />
+                        {/if}
+                        {#if layoutConfig.site.addCity_01 !== ''}
+                            {layoutConfig.site.addCity_01}
+                        {/if}
+                        {#if layoutConfig.site.addCity_01 && layoutConfig.site.addRegion_01 !== ''}
+                            , {layoutConfig.site.addRegion_01}
+                        {:else}
+                            {layoutConfig.site.addRegion_01}
+                        {/if}
+                        {#if layoutConfig.site.addPostalCode_01 !== ''}
+                            {layoutConfig.site.addPostalCode_01}
+                        {/if}
+                    </p>
+                </div>
             </div>
-            <div class="col-span-11">
-                <p class="m-0">
-                    {layoutConfig.site.addStreet1_01}, {layoutConfig.site.addStreet2_01}
-                    <br />
-                    {layoutConfig.site.addCity_01}, {layoutConfig.site.addRegion_01}
-                    {layoutConfig.site.addPostalCode_01}, {layoutConfig.site.addCountry_01}
-                    <br />
-                    {layoutConfig.site.urlAdd_01}
-                    <br />
-                    {layoutConfig.site.telNumber_01}
-                    <br />
-                    {layoutConfig.site.emailAdd_01}
-                </p>
-            </div>
-        </div>
+        {/if}
         <!-- note -->
-        <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
-            <div class="flex content-center items-center">
-                <span class="[&>svg]:h-10 [&>svg]:w-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512">
-                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/note-sticky?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                        <path
-                            d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H288V368c0-26.5 21.5-48 48-48H448V96c0-35.3-28.7-64-64-64H64zM448 352H402.7 336c-8.8 0-16 7.2-16 16v66.7V480l32-32 64-64 32-32z" />
-                    </svg>
-                </span>
+        {#if layoutConfig.site.vCardNote !== ''}
+            <div class="grid grid-cols-12 grid-rows-1 mb-8 gap-x-10">
+                <div class="flex content-center items-center">
+                    <span class="[&>svg]:h-10 [&>svg]:w-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com/icons/note-sticky?f=classic&s=solid License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H288V368c0-26.5 21.5-48 48-48H448V96c0-35.3-28.7-64-64-64H64zM448 352H402.7 336c-8.8 0-16 7.2-16 16v66.7V480l32-32 64-64 32-32z" />
+                        </svg>
+                    </span>
+                </div>
+                <div class="col-span-11">
+                    <p class="m-0">
+                        {layoutConfig.site.vCardNote}
+                    </p>
+                </div>
             </div>
-            <div class="col-span-11">
-                <p class="m-0">
-                    {layoutConfig.site.vCardNote}
-                </p>
-            </div>
-        </div>
+        {/if}
     </div>
 </div>
